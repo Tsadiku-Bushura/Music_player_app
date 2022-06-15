9b6da3ced0568f755ccc7edef35ec513e120f8c0
Girum13 <90162055+Girum13@users.noreply.github.com>
2022-06-15 16:12:53 -0700

    Adding app_drawer.dart

--- music_player_app/lib/widget/app_drawer.dart
+++ music_player_app/lib/widget/app_drawer.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
+import '../screen/about.dart';
+//import 'package:nav_item/screens/fav_items_screen.dart';
 
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          AppBar(
+            title: const Text("Menu"),
+          ),
+          Container(
+            //color: Color.fromARGB(255, 7, 65, 255),
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
