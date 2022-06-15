675e8d6ae056fba13670963a32d8340d0906a926
Zekarias <90303901+zeedology@users.noreply.github.com>
2022-06-16 02:16:42 +0300

    Update about.dart

--- music_player_app/lib/screen/about.dart
+++ music_player_app/lib/screen/about.dart
@@ -8,7 +8,7 @@ class AboutScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("About this App"),
+        title: const Text("About our App"),
       ),
       body: ListView(
         padding: const EdgeInsets.all(20),
